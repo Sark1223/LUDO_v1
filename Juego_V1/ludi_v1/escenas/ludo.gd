41d094761110ba83ea10fbfb93ddbb9039d8f827
Karla Judith Santos Rivera <105814864+Sark1223@users.noreply.github.com>
2024-09-25 12:41:47 -0700

    ubicacion de personajes de inicio
    Se ubico a los personajes en su base de inicio

--- Juego_V1/ludi_v1/escenas/ludo.gd
+++ Juego_V1/ludi_v1/escenas/ludo.gd
@@ -18,7 +18,6 @@ var tablero : Array
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	tablero.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,])
-	tablero.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,])
 	tablero.append([0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0,])
 	tablero.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,])
 	tablero.append([0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0,])
@@ -26,20 +25,44 @@ func _ready() -> void:
 	tablero.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,])
 	tablero.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,])
 	tablero.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,])
+	tablero.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,])
 	tablero.append([0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0,])
 	tablero.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,])
 	tablero.append([0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0,])
-	tablero.append([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,])
+	tablero.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,])
 
 
 	display_board()
 
+#func display_board():
+	#for y in tablero_tamanio:
+		#for x in tablero_tamanio:
+			#var holder = TEXTURE_HOLDER.instantiate()
+			#piezas.add_child(holder)
+			#holder.global_position = Vector2((-x * cell_with + (cell_with / 2))+300, (-y * cell_with - (cell_with / 2))+300)
+			#
+			#match tablero[x][y]:
+				#1:holder.texture = STATIC_CAT
 func display_board():
-	for y in tablero_tamanio:
-		for x in tablero_tamanio:
+	for y in range(tablero_tamanio):
+		for x in range(tablero_tamanio):
 			var holder = TEXTURE_HOLDER.instantiate()
 			piezas.add_child(holder)
-			holder.global_position = Vector2((-x * cell_with + (cell_with / 2))+300, (-y * cell_with - (cell_with / 2))+300)
 			
-			match tablero[x][y]:
+			if (y == 1):
+				holder.global_position = Vector2(((x * cell_with)  + (cell_with / 5)) - 300, ((y * cell_with) + (cell_with/3)) - 300)
+			elif ( y == 11):
+				holder.global_position = Vector2(((x * cell_with)  + (cell_with / 5)) - 300, ((y * cell_with) - (cell_with/3)) - 300)
+			else:
+				holder.global_position = Vector2(((x * cell_with)  + (cell_with / 5)) - 300, ((y * cell_with)) - 300)
+			
+			#elif (x == 9 or x == 11):
+				## Ajustar la posición de estas columnas en particular
+				#holder.global_position = Vector2(((x * cell_with) + (cell_with / 2)) - 315, ((y * cell_with) ) - 340)
+			#else:
+				## Posicionar normalmente las otras columnas
+				#holder.global_position = Vector2(((x * cell_with) + (cell_with / 2)) - 300, ((y * cell_with) - 40) - 300)
+			#
+			# Asignar textura según el valor en el tablero
+			match tablero[y][x]:
 				1:holder.texture = STATIC_CAT
