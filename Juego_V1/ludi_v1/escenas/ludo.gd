37a0d6896c56bc055d30c3a51996c536bfbad161
Karla Judith Santos Rivera <105814864+Sark1223@users.noreply.github.com>
2024-09-25 18:48:07 -0700

    personajes en tablero
    Tortuga y Sombrero

--- Juego_V1/ludi_v1/escenas/ludo.gd
+++ Juego_V1/ludi_v1/escenas/ludo.gd
@@ -8,6 +8,8 @@ const TEXTURE_HOLDER = preload("res://escenas/texture_holder.tscn")
 #Personajes
 const gato_salto  = preload("res://Rocky Roads/Rocky Roads/Personajes/gato_salto.png");
 const STATIC_CAT = preload("res://Rocky Roads/Rocky Roads/Personajes/static_cat.png")
+const SOMBRERO = preload("res://Rocky Roads/Rocky Roads/Personajes/sombrero.png")
+const TORTUGA = preload("res://Rocky Roads/Rocky Roads/Personajes/tortuga.png")
 
 @onready var piezas = $piezas
 @onready var posicion_mov = $posicion_mov
@@ -18,51 +20,37 @@ var tablero : Array
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	tablero.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,])
-	tablero.append([0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0,])
+	tablero.append([0, 1, 0, 1, 0, 0, 0, 0, 0, 2, 0, 2, 0,])
 	tablero.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,])
-	tablero.append([0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0,])
-	tablero.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,])
-	tablero.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,])
+	tablero.append([0, 1, 0, 1, 0, 0, 0, 0, 0, 2, 0, 2, 0,])
 	tablero.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,])
 	tablero.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,])
 	tablero.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,])
-	tablero.append([0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0,])
 	tablero.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,])
-	tablero.append([0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0,])
+	tablero.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,])
+	tablero.append([0, 3, 0, 3, 0, 0, 0, 0, 0, 1, 0, 1, 0,])
+	tablero.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,])
+	tablero.append([0, 3, 0, 3, 0, 0, 0, 0, 0, 1, 0, 1, 0,])
 	tablero.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,])
 
 
 	display_board()
 
-#func display_board():
-	#for y in tablero_tamanio:
-		#for x in tablero_tamanio:
-			#var holder = TEXTURE_HOLDER.instantiate()
-			#piezas.add_child(holder)
-			#holder.global_position = Vector2((-x * cell_with + (cell_with / 2))+300, (-y * cell_with - (cell_with / 2))+300)
-			#
-			#match tablero[x][y]:
-				#1:holder.texture = STATIC_CAT
 func display_board():
 	for y in range(tablero_tamanio):
 		for x in range(tablero_tamanio):
 			var holder = TEXTURE_HOLDER.instantiate()
 			piezas.add_child(holder)
 			
-			if (y == 1):
+			if (y == 1):#Acomodar los personajes de arriba correctamente en la base
 				holder.global_position = Vector2(((x * cell_with)  + (cell_with / 5)) - 300, ((y * cell_with) + (cell_with/3)) - 300)
-			elif ( y == 11):
+			elif ( y == 11):#Acomodar los personajes de abajo correctamente en la base
 				holder.global_position = Vector2(((x * cell_with)  + (cell_with / 5)) - 300, ((y * cell_with) - (cell_with/3)) - 300)
 			else:
 				holder.global_position = Vector2(((x * cell_with)  + (cell_with / 5)) - 300, ((y * cell_with)) - 300)
 			
-			#elif (x == 9 or x == 11):
-				## Ajustar la posición de estas columnas en particular
-				#holder.global_position = Vector2(((x * cell_with) + (cell_with / 2)) - 315, ((y * cell_with) ) - 340)
-			#else:
-				## Posicionar normalmente las otras columnas
-				#holder.global_position = Vector2(((x * cell_with) + (cell_with / 2)) - 300, ((y * cell_with) - 40) - 300)
-			#
 			# Asignar textura según el valor en el tablero
 			match tablero[y][x]:
 				1:holder.texture = STATIC_CAT
+				2:holder.texture = SOMBRERO
+				3:holder.texture = TORTUGA
